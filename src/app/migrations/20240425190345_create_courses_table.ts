0d7db1d8b981d63ec2af22c28c9c7d9526a6d7a2
guilhermigg <guilhermigg.e@gmail.com>
2024-04-30 09:48:09 -0300

    update: converted migration.js to .ts

---
+++ src/app/migrations/20240425190345_create_courses_table.ts
@@ -0,0 +1,18 @@
+import type { Knex } from "knex";
+
+
+export async function up(knex: Knex) : Promise<void> {
+    return knex.schema.createTable('courses', (table) => {
+        table.increments('id').primary();
+        table.string('title').notNullable();
+        table.integer('price');
+        table.text('description');
+        table.boolean('active');
+        table.timestamps(true, true); 
+    });
+};
+
+
+export async function down(knex: Knex) : Promise<void> {
+    return knex.schema.dropTableIfExists('courses');
+};
