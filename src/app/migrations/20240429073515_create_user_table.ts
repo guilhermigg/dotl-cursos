a85aeaadb6829cca95ff08d2e72bad7a2eb9a182
guilhermigg <guilhermigg.e@gmail.com>
2024-04-30 09:46:43 -0300

    new: create user table migration

---
+++ src/app/migrations/20240429073515_create_user_table.ts
@@ -0,0 +1,19 @@
+import type { Knex } from "knex";
+
+
+export async function up(knex: Knex) : Promise<void> {
+    return knex.schema.createTable('users', table => {
+        table.increments('id').primary();
+        table.string('name').notNullable();
+        table.string('email').notNullable();
+        table.string('password').notNullable();
+        table.boolean('active');
+        table.timestamps(true, true); 
+    })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    return knex.schema.dropTableIfExists('users');
+}
+
