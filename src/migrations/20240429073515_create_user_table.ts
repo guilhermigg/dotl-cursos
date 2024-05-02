68bef83bdbf452315e9cfbe8597612fb0afde0f3
guilhermigg <guilhermigg.e@gmail.com>
2024-05-02 11:23:34 -0300

    moved migrations to the correct folder

--- src/migrations/20240429073515_create_user_table.ts
+++ src/migrations/20240429073515_create_user_table.ts
@@ -1,13 +1,14 @@
 import type { Knex } from "knex";
 
-
+// roles: 1- admin / 2- teacher / 3- student
 export async function up(knex: Knex) : Promise<void> {
     return knex.schema.createTable('users', table => {
         table.increments('id').primary();
         table.string('name').notNullable();
         table.string('email').notNullable();
         table.string('password').notNullable();
-        table.boolean('active');
+        table.string('role').defaultTo(3);
+        table.boolean('active').defaultTo(true);
         table.timestamps(true, true); 
     })
 }
