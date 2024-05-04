a6d13404b6836e9eb88b33ef51d071d973b2b2d0
guilhermigg <guilhermigg.e@gmail.com>
2024-05-04 18:11:22 -0300

    add: new thumbnail column in the courses table

--- src/migrations/20240425190345_create_courses_table.ts
+++ src/migrations/20240425190345_create_courses_table.ts
@@ -5,6 +5,7 @@ export async function up(knex: Knex) : Promise<void> {
     return knex.schema.createTable('courses', (table) => {
         table.increments('id').primary();
         table.string('title').notNullable();
+        table.string('thumbnail');
         table.integer('price');
         table.text('description');
         table.boolean('active');
