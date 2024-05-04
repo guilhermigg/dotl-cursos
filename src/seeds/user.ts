76d35dd18e46a71181717ad05037d43b634d9a35
guilhermigg <guilhermigg.e@gmail.com>
2024-05-04 18:13:29 -0300

    update: user seeding

--- src/seeds/user.ts
+++ src/seeds/user.ts
@@ -1,7 +1,9 @@
 import { Knex } from "knex";
+import { hashPassword } from "../app/api/helpers/bcryptHandler";
 
 export async function seed(knex: Knex): Promise<void> {
+    const hashedPassword = await hashPassword("admin123")
     await knex("users").insert([
-        { name: "user user", email: "user@email.com", password: "helloworld", active: true }
+        { name: "Admin User", email: "user@email.com", password: hashedPassword, role: "1", active: true }
     ]);
 };
